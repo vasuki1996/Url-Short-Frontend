312c6607c7b834425d0cc2a602052e94568113f3
Vasuki Gopinath <vasuki_gopinath@thbs.india.com>
2019-12-09 15:38:42 +0530

    redirection fix

---
+++ precache-manifest.340ecf5541b5572c100352299ed7611f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41c05ba46655f4e54c8fa59348406abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8da37eb0f6e286b94e43",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bdff2019c15b9b9544d1",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8da37eb0f6e286b94e43",
+    "url": "/static/js/2.77965be2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.77965be2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdff2019c15b9b9544d1",
+    "url": "/static/js/main.72bd57eb.chunk.js"
+  },
+  {
+    "revision": "28073e45edd7f285705e",
+    "url": "/static/js/runtime-main.e89fc5be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
